f321eea250cb8245a0d20ced86c1f0c0bfff1fe4
Erlend Oftedal <erlend@oftedal.no>
2020-04-22 21:22:18 +0200

    Add server identifier

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const port = 8080;
+const iam = require("crypto").randomBytes(5).toString("hex");
 
 
 let server = http.createServer((req, res) => {
@@ -7,6 +8,7 @@ let server = http.createServer((req, res) => {
         console.log(new Date().toISOString() + " " + res.statusCode + " " + req.url)
     );
     res.setHeader("Content-Type", "text/plain");
+    res.setHeader("x-i-am", iam);
 	res.end(req.method + " " + req.url + "\n" + JSON.stringify(req.headers, true) + "\n");
 }).listen(port, () => {
 	console.log(`Listening on ${port}`)
